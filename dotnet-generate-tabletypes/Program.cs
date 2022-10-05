707650cc5ec12d27ff2679dc2c6e0239b737fdc0
Nick M Cummins <nickmcummins@gmail.com>
2022-10-05 10:32:40 -0700

    implementing a CLI using System.CommandLine

--- dotnet-generate-tabletypes/Program.cs
+++ dotnet-generate-tabletypes/Program.cs
@@ -1,5 +1,15 @@
 ﻿using DotNet.GenerateTableTypes;
+using System.CommandLine;
 
-var tablesDir = args[0];
-var tableTypeGenerator = new TableTypeGenerator(tablesDir);
-tableTypeGenerator.WriteFiles();
+var command = new RootCommand("Generate SQL Server table types from table definitions.");
+var typesDirArgument = new Argument<string>() { Name = "Tables directory" };
+var typeOptionsOption = new Option<IEnumerable<string>>("--type-options") { AllowMultipleArgumentsPerToken = true };
+command.Add(typesDirArgument);
+command.Add(typeOptionsOption);
+command.SetHandler((tablesDir, typesOptions) => 
+{ 
+    var ttGenerator = new TableTypesGenerator(tablesDir, typesOptions);
+    ttGenerator.WriteFiles();
+}, typesDirArgument, typeOptionsOption);
+
+await command.InvokeAsync(args);
